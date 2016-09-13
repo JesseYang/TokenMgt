3a0d484c7a9a7644c5d49dd97b8d28cce93d4a7a
JesseYang <jesse.yang1985@gmail.com>
2016-09-13 15:06:31 +0800

    box.com get access token

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,13 +1,8 @@
 class RedirectsController < ApplicationController
 
   def box
-    Rails.logger.info "AAAAAAAAAAAAAAAAAAAAA"
-    Rails.logger.info params.inspect
-    Rails.logger.info "AAAAAAAAAAAAAAAAAAAAA"
-
     account = Account.where(id: params[:state]).first
     retval = account.get_access_token(params[:code])
-
-    render text: retval.inspect
+    redirect_to "/" and return
   end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -3,13 +3,14 @@ class Account
   include Mongoid::Timestamps
 
   include HTTParty
-  base_uri 'https://api.box.com'
+  base_uri 'https://api.box.com/'
 
   field :platform, type: String
   field :account, type: String
   field :password, type: String
   field :access_token, type: String
   field :refresh_token, type: String
+  field :token_updated_at, type: Date
 
   BOX_CLIENT_ID = "buq4gus2fhoariwcaq0r15nf6l2u8auf"
   BOX_CLIENT_SECRET = "gi5W2ysE3Q0CtNhoqbVHjD6GMSaaycPQ"
@@ -36,11 +37,20 @@ class Account
 
   def get_access_token(code)
     options = {
-      grant_type: "authorization_code",
-      code: code,
-      client_id: BOX_CLIENT_ID,
-      client_secret: BOX_CLIENT_SECRET
+      body: {
+        grant_type: "authorization_code",
+        code: code,
+        client_id: BOX_CLIENT_ID,
+        client_secret: BOX_CLIENT_SECRET
+      }
     }
-    retval = self.class.post("oauth2/token", options)
+    response = self.class.post("/oauth2/token", options)
+    data = JSON.parse(response.body)
+    self.update_attributes(
+      {
+        access_token: data["access_token"],
+        refresh_token: data["refresh_token"],
+        token_updated_at: Time.now
+      })
   end
 end
