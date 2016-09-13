0130f7b396dd53de56a8e7464965f48de4b2ba90
JesseYang <jesse.yang1985@gmail.com>
2016-09-13 16:59:21 +0800

    update

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,16 @@
 class AccountsController < ApplicationController
 
   def create
-  	Account.create_account(params[:platform], params[:account], params[:password])
-  	redirect_to "/?platform=#{params[:platform]}" and return
+    Account.create_account(params[:platform], params[:account], params[:password])
+    redirect_to "/?platform=#{params[:platform]}" and return
+  end
+
+  def index
+    box_accounts = Account.where(platform: "box")
+    box_accounts.each do |e|
+      e.refresh_tokens
+    end
+    access_tokens = box_accounts.map { |e| e.access_token }
+    render text: access_tokens.join(',') and return
   end
 end
