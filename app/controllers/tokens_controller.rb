5638cbec62f57d7ed71e0fe6ea999d85189913da
JesseYang <jesse.yang1985@gmail.com>
2017-02-28 23:24:20 +0800

    u

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,12 +1,16 @@
 class TokensController < ApplicationController
 
   def index
-    box_accounts = Account.where(platform: "box", account: /box*/)
-    # box_accounts.each do |e|
-    #   e.refresh_tokens
-    # end
+  	a1 = Account.where(platform: "box", account: "box0@b-fox.cn").first
+  	a2 = Account.where(platform: "box", account: "box1@b-fox.cn").first
+  	a3 = Account.where(platform: "box", account: "box2@b-fox.cn").first
+  	a4 = Account.where(platform: "box", account: "box3@b-fox.cn").first
+  	a5 = Account.where(platform: "dropbox", account: "box0@b-fox.cn").first
+
+
+    # box_accounts = Account.where(platform: "box", account: /box*/)
+    box_accounts = [a1, a2, a3, a4, a5]
     access_tokens = box_accounts.map { |e| e.access_token } .select { |e| e.present? }
-    # render text: "AAA,BBB,CCC,DDD,EEE" and return
     render text: access_tokens.join(',') and return
   end
 end
